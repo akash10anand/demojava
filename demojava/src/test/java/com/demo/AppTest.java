4dd52dab12c3739191c0a26392eb9466bf3bce3f
Akash Anand <akash10.anand@gmail.com>
2020-12-16 17:22:22 +0000

    done some minor changes

--- demojava/src/test/java/com/demo/AppTest.java
+++ demojava/src/test/java/com/demo/AppTest.java
@@ -8,7 +8,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
-import org.apache.logging.log4j.core.config.plugins.Plugin;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
